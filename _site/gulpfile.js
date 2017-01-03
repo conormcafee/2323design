65e799d0c24f64c853de4b3a789c053f74d55046
conor-mcafee <hello@conormcafee.co.uk>
2017-01-03 21:26:33 +0000

    Progress on Header and Nav

--- _site/gulpfile.js
+++ _site/gulpfile.js
@@ -1,24 +1,69 @@
 /*!
  * gulp
- * npm install gulp gulp-postcss cssnano autoprefixer --save-dev
+ * npm install gulp gulp-ruby-sass gulp-autoprefixer gulp-livereload gulp-cssnano gulp-concat gulp-uglify gulp-notify gulp-rename gulp-cache del gulp-svgmin gulp-inline-source --save-dev
  */
 
 // Load plugins
-var postcss = require('gulp-postcss'),
-    gulp = require('gulp'),
-    cssnext = require('postcss-cssnext');
-    cssnano = require('cssnano');
-    autoprefixer = require('autoprefixer');
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    cssnano = require('gulp-cssnano'),
+    prefix = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    del = require('del'),
+    svgmin = require('gulp-svgmin'),
+    inlinesource = require('gulp-inline-source'),
+    livereload = require('gulp-livereload');
 
-// CSS
-gulp.task('css', function () {
-  var processors = [
-    cssnext({browsers: ['last 4 versions']}),
-    cssnano({ autoprefixer: false }),
-  ];
-  return gulp.src('build/css/*.css')
-    .pipe(postcss(processors))
-    .pipe(gulp.dest('assets/css'));
+// Sass
+gulp.task('sass', function() {
+    return sass(['build/sass/development.scss', 'build/sass/critical.scss', 'build/sass/style.scss'], { style: 'expanded' })
+    .pipe(prefix({ browsers: ['ie 9', 'last 4 versions'] }))
+    .pipe(gulp.dest('assets/css'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(cssnano())
+    .pipe(gulp.dest('assets/css'))
+    .pipe(notify({ message: 'Styles task complete' }))
+    .pipe(livereload());
 });
 
-// W
+// Scripts
+gulp.task('scripts', function() {
+  return gulp.src(['build/js/vendor/modernizr.custom.js', 'build/js/vendor/jquery-2.2.3.js', 'build/js/vendor/echo.js', 'build/js/vendor/unslider.js', 'build/js/scripts.js'])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('assets/js'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest('assets/js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+// SVGOMG
+gulp.task('svg', function () {
+  return gulp.src('build/svg/*.svg')
+    .pipe(svgmin({
+      js2svg: {
+        pretty: true
+      }
+    }))
+  .pipe(gulp.dest('assets/svg'));
+});
+
+// Critical CSS
+gulp.task('is', function () {
+  return gulp.src('craft/templates/_critical/_critical-css.html')
+    .pipe(inlinesource())
+    .pipe(gulp.dest('craft/templates/_includes'));
+});
+
+// Watch
+gulp.task('watch', function() {
+    livereload.listen();
+    gulp.watch('build/sass/**/*.scss', ['sass']);
+    gulp.watch('build/images/*', ['images']);
+    gulp.watch('build/svg/*', ['svg']);
+    gulp.watch('build/js/*.js', ['scripts']);
+});
